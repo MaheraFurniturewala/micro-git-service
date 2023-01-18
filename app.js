7a38796b8fad400b803e288b714af2bbf973e35e
MaheraFurniturewala <maherafurniturewala1111@gmail.com>
2023-01-18 10:36:05 +0530

    Authenticate app

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 /**
  * @param {import('probot').Probot} app
  */
-module.exports = (app) => {
+
+const { Octokit } = require("@octokit/rest");
+const { createAppAuth } = require("@octokit/auth-app");
+
+require("dotenv").config()
+
+module.exports = async (app, {getRouter}) => {
   app.log("Yay! The app was loaded!");
+
+  const server = getRouter("/");
+
+  server.get("/", async (req, res) => {
+    res.send("Hello");
+  });
+
+  server.get("/repo/:owner/:repo", async (req, res) => {
+    const { owner, repo } = req.params;
+  
+    const installationOctokit = new Octokit({
+      authStrategy: createAppAuth,
+      auth: {
+        appId: process.env.APP_ID,
+        privateKey: process.env.PRIVATE_KEY,
+        installationId: 33254077,
+      },
+    });
+  
+    const repos = await installationOctokit.request(`GET /repos/${owner}/${repo}`)
+    res.json(repos);
+  });
 
   app.on("issues.opened", async (context) => {
     return context.octokit.issues.createComment(
